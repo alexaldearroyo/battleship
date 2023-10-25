c79f8a79e446a1f235c16d64c55f57361e65ea17
alexaldearroyo <alexaldearroyo@gmail.com>
2023-10-25 22:33:53 +0900

    Board grid implemented

--- js/main.js
+++ js/main.js
@@ -4,6 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const startGameButton = document.getElementById('startGameButton');
     const gameContainer = document.getElementById('gameContainer');
 
+    // Función para generar el tablero
+    function generateBoard(gridElement) {
+        for (let x = 0; x < 10; x++) {
+            for (let y = 0; y < 10; y++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.dataset.x = x;
+                cell.dataset.y = y;
+                gridElement.appendChild(cell);
+            }
+        }
+    }
+
+    // Generar tableros
+    const playerGrid = document.querySelector('#player-board .grid');
+    const machineGrid = document.querySelector('#machine-board .grid');
+    generateBoard(playerGrid);
+    generateBoard(machineGrid);
+
     startGameButton.addEventListener('click', function() {
         // Mostrar el contenedor de los tableros
         gameContainer.style.display = 'flex';
